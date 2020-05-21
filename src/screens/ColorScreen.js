98560a370d4b548f5593118f6923f032a8364465
erica2605@live.com <erica2605@live.com>
2020-05-21 15:58:22 -0400

    added random color function

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -5,8 +5,18 @@ const ColorScreen = () => {
   return (
     <View>
       <Button title="Add a Color"/>
+      <View
+        style={{ height: 100, width: 100, backgroundColor: randomRGB()}}/>
     </View>
   )
+}
+
+const randomRGB = () => {
+  const red = Math.floor(Math.random() * 256)
+  const green = Math.floor(Math.random() * 256)
+  const blue = Math.floor(Math.random() * 256)
+
+  return `rgb(${red}, ${green}, ${blue})`
 }
 
 const styles = StyleSheet.create({})
