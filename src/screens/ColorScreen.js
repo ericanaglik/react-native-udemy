2619f6aaa5089ce16e08035a34e8020302f125bf
erica2605@live.com <erica2605@live.com>
2020-05-21 20:02:42 -0400

    add color flatlist working

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import { View, StyleSheet, Button } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, Button, FlatList } from 'react-native'
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
   return (
     <View>
-      <Button title="Add a Color"/>
-      <View
-        style={{ height: 100, width: 100, backgroundColor: randomRGB()}}/>
+      <Button 
+        title="Add a Color"
+        onPress={() => {
+          setColors([...colors, randomRGB()])
+        }}
+      />
+      
+      <FlatList
+        keyExtractor={item => item}
+        data={colors}
+        renderItem={({item}) => {
+          return (
+          <View style={{ height: 100, flex: 1, backgroundColor: item}}/>
+          )
+        }}
+      />
     </View>
   )
 }
