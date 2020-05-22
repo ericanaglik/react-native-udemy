cc25a5133d71e58175fb0243775d2516a8c0cf5a
erica2605@live.com <erica2605@live.com>
2020-05-21 20:51:47 -0400

    square color change working

--- App.js
+++ App.js
@@ -5,12 +5,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import ImageScreen from './src/screens/ImageScreen'
 import ColorScreen from './src/screens/ColorScreen'
 import HomeScreen from './src/screens/HomeScreen'
+import SquareScreen from './src/screens/SquareScreen'
 
 const navigator = createStackNavigator(
   {
     Color: ColorScreen,
     Image: ImageScreen,
-    Home: HomeScreen
+    Home: HomeScreen,
+    Square: SquareScreen
   },
   {
     initialRouteName: "Home",
