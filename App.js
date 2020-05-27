562473e865011db31673a144ddeb0419a5858df6
erica2605@live.com <erica2605@live.com>
2020-05-27 17:57:27 -0400

    coded box screen template

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import ColorScreen from './src/screens/ColorScreen'
 import HomeScreen from './src/screens/HomeScreen'
 import SquareScreen from './src/screens/SquareScreen'
 import TextScreen from './src/screens/TextScreen'
+import BoxScreen from './src/screens/BoxScreen'
 
 const navigator = createStackNavigator(
   {
@@ -14,7 +15,8 @@ const navigator = createStackNavigator(
     Image: ImageScreen,
     Home: HomeScreen,
     Square: SquareScreen,
-    Text: TextScreen
+    Text: TextScreen,
+    Box: BoxScreen
   },
   {
     initialRouteName: "Home",
