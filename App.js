75c20945ecd586432cbde72fa2b388305aaeced6
erica2605@live.com <erica2605@live.com>
2020-05-27 17:03:26 -0400

    coded textscreen boilerplate

--- App.js
+++ App.js
@@ -6,13 +6,15 @@ import ImageScreen from './src/screens/ImageScreen'
 import ColorScreen from './src/screens/ColorScreen'
 import HomeScreen from './src/screens/HomeScreen'
 import SquareScreen from './src/screens/SquareScreen'
+import TextScreen from './src/screens/TextScreen'
 
 const navigator = createStackNavigator(
   {
     Color: ColorScreen,
     Image: ImageScreen,
     Home: HomeScreen,
-    Square: SquareScreen
+    Square: SquareScreen,
+    Text: TextScreen
   },
   {
     initialRouteName: "Home",
